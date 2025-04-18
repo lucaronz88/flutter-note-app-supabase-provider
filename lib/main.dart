a71cc4657abd431e19ee8272f7dfa7252b95209a
lucaronz88 <lucaronz88@gmail.com>
2025-04-18 16:24:26 +0700

    Added provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
