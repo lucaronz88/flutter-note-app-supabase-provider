333b72437a1caee901cec152906ba71f464d35ad
lucaronz88 <lucaronz88@gmail.com>
2025-04-18 16:23:29 +0700

    Added services for notes

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:notes_app_supabase_provider/models/note.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
@@ -21,7 +22,7 @@ class SupabaseService {
   AUTH METHODS
 
   */
-  
+
   // Sign up
   Future<void> signUp(String email, String password) async {
     final response = await _client.auth.signUp(
@@ -63,5 +64,53 @@ class SupabaseService {
 
   */
 
-  // WIP
+  // Get notes streams
+  Stream<List<Note>> getNotesStream() {
+    return _client
+        .from('notes')
+        .stream(primaryKey: ['id'])
+        .order('updated_at')
+        .map((response) {
+          // Convert the response to a list of notes
+          return response.map((note) {
+            // Convert the note json to a Note object
+            return Note.fromJson(note);
+          }).toList();
+        });
+  }
+
+  // Get single note stream
+  Stream<Note?> getNoteStream(String noteId) {
+    return _client
+        .from('notes')
+        .stream(primaryKey: ['id'])
+        .eq('id', noteId)
+        .map((response) {
+          // Convert the response to a list of notes
+          return response.isNotEmpty ? Note.fromJson(response.first) : null;
+        });
+  }
+
+  // Create note
+  Future<void> createNote(String title, String content) async {
+    await _client.from('notes').insert({
+      'title': title,
+      'content': content,
+      'user_id': currentUserId,
+    });
+  }
+
+  // Update note
+  Future<void> updateNote(String noteId, String title, String content) async {
+    await _client.from('notes').update({
+      'title': title,
+      'content': content,
+      'updated_at': DateTime.now().toIso8601String(),
+    }).eq('id', noteId);
+  }
+
+  // Delete note
+  Future<void> deleteNote(String noteId) async {
+    await _client.from('notes').delete().eq('id', noteId);
+  }
 }
